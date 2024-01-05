7940e543781fc7e9f88e5e194ab71844e409d014
Mathan-V <marimathan1998@gmail.com>
2024-01-05 22:30:48 +0530

    Category Design Updated

--- apv/urls.py
+++ apv/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('home',views.home,name="home"),
-    path('register',views.register,name="register")
+    path('',views.home,name="home"),
+    path('register',views.register,name="register"),
+    path('collections',views.collections,name="collections")
 ]
--- apv/views.py
+++ apv/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from . models import *
 # Create your views here.
 def home(request):
     return render(request,"shop/index.html")
 def register(request):
     return render(request,"shop/register.html")
+def collections(request):
+    catagory = Catagory.objects.filter(status=0)
+    return render(request,"shop/collections.html",{"catagory":catagory})
