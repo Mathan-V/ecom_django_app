07b4df5920f2a11e52afd43520cda684c887fda4
Mathan-V <marimathan1998@gmail.com>
2024-01-06 14:22:18 +0530

    Product Design Updated

--- apv/urls.py
+++ apv/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('',views.home,name="home"),
     path('register',views.register,name="register"),
-    path('collections',views.collections,name="collections")
+    path('collections',views.collections,name="collections"),
+    path('collections/<str:name>',views.collectionsview,name="collections")
 ]
--- apv/views.py
+++ apv/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from . models import *
+from django.contrib import messages
+
 # Create your views here.
 def home(request):
     return render(request,"shop/index.html")
@@ -8,3 +10,12 @@ def register(request):
 def collections(request):
     catagory = Catagory.objects.filter(status=0)
     return render(request,"shop/collections.html",{"catagory":catagory})
+
+
+def collectionsview(request,name):
+    if (Catagory.objects.filter(name=name,status=0)):
+        products = Product.objects.filter(catagory__name=name)
+        return render(request,"shop/products/index.html",{"products":products})
+    else:
+        messages.warning(request,"No Such Catagory Found")
+        return redirect('collections')
