49ac00d550e4041b1c9b36561bab7d07550fe5b3
Mathan-V <marimathan1998@gmail.com>
2024-01-09 10:37:14 +0530

    Add to Cart New Features Updated

--- apv/urls.py
+++ apv/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('',views.home,name="home"),
     path('register',views.register,name="register"),
     path('collections',views.collections,name="collections"),
-    path('collections/<str:name>',views.collectionsview,name="collections")
+    path('collections/<str:name>',views.collectionsview,name="collections"),
+    path('collections/<str:cname>/<str:pname>',views.product_details,name="product_details")
 ]
--- apv/views.py
+++ apv/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from . models import *
 from django.contrib import messages
+from django.http import HttpResponse 
 
 # Create your views here.
 def home(request):
@@ -15,7 +16,19 @@ def collections(request):
 def collectionsview(request,name):
     if (Catagory.objects.filter(name=name,status=0)):
         products = Product.objects.filter(catagory__name=name)
-        return render(request,"shop/products/index.html",{"products":products})
+        return render(request,"shop/products/index.html",{"products":products,"catagory_name":name})
     else:
         messages.warning(request,"No Such Catagory Found")
+        return redirect('collections')
+
+def product_details(request,cname,pname):
+    if Catagory.objects.filter(name=cname,status=0):
+        if Product.objects.filter(name=pname,status=0):
+            products = Product.objects.filter(name=pname,status=0).first()
+            return render(request,"shop/products/product_details.html",{"products":products})
+        else:
+            messages.error(request,"No Such Catagory Found")
+            return redirect('collections')
+    else:
+        messages.error(request,"No Such Catagory Found")
         return redirect('collections')
